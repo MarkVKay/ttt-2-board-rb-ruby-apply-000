cc182edff006f15656adb55c21f74c2c3b13f2f6
glorious-sequence-0954 <markvaughankay@gmail.com>
2020-09-21 22:13:51 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,14 +1,14 @@
-row = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 # Position 1: Top Left 
-row [0]
+board [0]
 
 # Position 2:  Top Middle 
-row [1]
+board [1]
 
 #  Position 3: Top Right
-row [2]
+board [2]
 
 #  Move X to the Position 2, Middle
-row[1] = "X"
+board[1] = "X"
 
