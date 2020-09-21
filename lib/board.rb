c7824f2a23685efd01b5ff4c7b3bb1cd0fbcdd02
glorious-sequence-0954 <markvaughankay@gmail.com>
2020-09-21 22:08:35 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,12 @@
-# Define the variable board below.
+row = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+# Position 1: Left 
+row [0]
+
+# Position 2: Middle 
+row [1]
+
+#  Position 3: Right
+row [2]
+
+
