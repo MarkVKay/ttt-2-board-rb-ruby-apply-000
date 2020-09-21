aed84e1bd43930bc01c811596a9b8c826d428e39
glorious-sequence-0954 <markvaughankay@gmail.com>
2020-09-21 22:11:33 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,12 +1,14 @@
 row = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-# Position 1: Left 
+# Position 1: Top Left 
 row [0]
 
-# Position 2: Middle 
+# Position 2:  Top Middle 
 row [1]
 
-#  Position 3: Right
+#  Position 3: Top Right
 row [2]
 
+#  Move X to the Position 2, Middle
+row[1] = "X"
 
